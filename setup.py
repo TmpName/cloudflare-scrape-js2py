7812ba564e24ce67708fef654238702c91155453
VeNoMouS <venom@gen-x.co.nz>
2019-04-01 22:24:34 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
   name = 'cfscrape',
   packages = ['cfscrape'],
   version = VERSION,
-  description = 'A simple Python module to bypass Cloudflare\'s anti-bot page. See https://github.com/venomous/cloudflare-scrape-js2py for more information.',
+  description = 'A simple Python module to bypass Cloudflare\'s anti-bot page using js2py. See https://github.com/venomous/cloudflare-scrape-js2py for more information.',
   author = 'VeNoMous',
   author_email = 'venom@gen-x.co.nz',
   url = 'https://github.com/venomous/cloudflare-scrape-js2py',
   keywords = ['cloudflare', 'scraping'],
   include_package_data = True,
-  install_requires = ['requests >= 2.0.0']
+  install_requires = ['requests >= 2.0.0', 'js2py >= 0.60']
 )
